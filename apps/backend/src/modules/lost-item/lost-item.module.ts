62d03c61541535af6db724a58d9075ddeaeb18cf
IT18 <IT18@SJA.COM>
2026-04-26 20:40:39 +0700

    feat(lost-item): register FoundItemClaim, LostItemStatusLog, FoundClaimService in module

--- apps/backend/src/modules/lost-item/lost-item.module.ts
+++ apps/backend/src/modules/lost-item/lost-item.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LostItemReport } from './entities/lost-item-report.entity';
+import { FoundItemClaim } from './entities/found-item-claim.entity';
+import { LostItemStatusLog } from './entities/lost-item-status-log.entity';
 import { LostItemService } from './lost-item.service';
+import { FoundClaimService } from './found-claim.service';
 import { LostItemController } from './lost-item.controller';
+import { FoundClaimController } from './found-claim.controller';
 import { Ticket } from '../ticketing/entities/ticket.entity';
 import { User } from '../users/entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
@@ -10,12 +14,12 @@ import { AuditModule } from '../audit/audit.module';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([LostItemReport, Ticket, User]),
+        TypeOrmModule.forFeature([LostItemReport, FoundItemClaim, LostItemStatusLog, Ticket, User]),
         AuthModule,
         AuditModule,
     ],
-    controllers: [LostItemController],
-    providers: [LostItemService],
-    exports: [LostItemService],
+    controllers: [LostItemController, FoundClaimController],
+    providers: [LostItemService, FoundClaimService],
+    exports: [LostItemService, FoundClaimService],
 })
-export class LostItemModule { }
+export class LostItemModule {}
